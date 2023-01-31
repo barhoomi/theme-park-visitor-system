6b61a43c6d9dbe1af657194cf4942aaf605724f9
d7mee-00 <123971801+d7mee-00@users.noreply.github.com>
2023-01-31 20:32:57 +0300

    Update visitorType.java

--- src/visitorType.java
+++ src/visitorType.java
@@ -4,11 +4,10 @@ public class visitorType {
 	private int num_visitors;
 	private LinkedList<visitorInfo> visitList;
 
-	public visitorType(int type, int num_visitors, LinkedList<visitorInfo> visitList) {
-		super();
-		this.type = type;
-		this.num_visitors = num_visitors;
-		this.visitList = visitList;
-	}
+	public visitotType() {
+		this.type = 0;
+		this.num_visitors = 0;
+		visitList = new LinkedList();
+	} 
 
 }
