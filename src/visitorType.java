76c76151bd6fb9734b35c576fe96931d9aaf3df9
YS-AJ <124058670+YS-AJ@users.noreply.github.com>
2023-01-31 16:35:24 +0300

    Update visitorType.java
    info

--- src/visitorType.java
+++ src/visitorType.java
@@ -2,9 +2,9 @@
 public class visitorType {
 	int type;
 	int num_visitors;
-	LinkedList<visitor> visitList;
+	LinkedList<visitorInfo> visitList;
 
-	public visitorType(int type, int num_visitors, LinkedList<visitor> visitList) {
+	public visitorType(int type, int num_visitors, LinkedList<visitorInfo> visitList) {
 		super();
 		this.type = type;
 		this.num_visitors = num_visitors;
