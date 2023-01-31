77e8aca9c3d6c4a8348a11dcc695f3e931ba4e58
AK7Amin <124058082+AK7Amin@users.noreply.github.com>
2023-01-31 17:38:22 +0300

    Update ThemeParkADT.java

--- src/ThemeParkADT.java
+++ src/ThemeParkADT.java
@@ -1,15 +1,31 @@
 
 public class ThemeParkADT {
-	private rgnInfo regionArray [];
-	private int k ;
-	private rgnInfo rgnSortedArray [];
-	private int r ;
-	private LinkedList <visitotType> visitors;
-	private LinkedList <visitorInfo> vips;
-	
-	
-	public void readFileAndAnalyse(String f){
-		
+	public class ThemeParkADT {
+	private rgnInfo regionArray[];
+	private final int k = 100;
+	private rgnInfo rgnSortedArray[];
+	private int r;
+	private LinkedList<visitorInfo> visitors;
+	private LinkedList<visitorInfo> vips;
+
+	public void readFileAndAnalyse(String f) throws IOException, ClassNotFoundException {
+		regionArray = new rgnInfo[k];
+		rgnSortedArray = new rgnInfo[k];
+		vips = new LinkedList<visitorInfo>();
+		visitors = new LinkedList<visitorInfo>();
+		File inFile = new File(f);
+		FileInputStream inFileStream = new FileInputStream(inFile);
+		ObjectInputStream inObjectStream = new ObjectInputStream(inFileStream);
+		visitorInfo v;
+		while (true) {
+			v = (visitorInfo) inObjectStream.readObject();
+			visitors.insert(v);
+			if (v.type == 0) {
+				vips.insert(v);
+				regionArray[v.region].vtype[0].visitList.insert(v);
+			} else
+				regionArray[v.region].vtype[1].visitList.insert(v);
+		}
 	}
 	
 	public void searchVISITOR(String lName){
