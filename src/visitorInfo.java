03ef84f86389a768dd8b18a6837baffd7766e18e
barhoomi <ialkhayatt@outlook.com>
2023-02-01 14:52:20 +0300

    Add printVisitorInfo method

--- src/visitorInfo.java
+++ src/visitorInfo.java
@@ -8,12 +8,20 @@ public class visitorInfo {
 		fName = info[0];
 		lName = info[1];
 		region = Integer.parseInt(info[2]);
-		phone = info[3];
-		type = Integer.parseInt(info[4]);
+		phone = info[4];
+		type = Integer.parseInt(info[3]);
 		order = new ArrayStack<>(info.length - 5);
 		for(int i = 5; i< info.length; i++){
 			order.push(Integer.parseInt(info[i]));
 		}
 	}
-	
+
+	public void printVisitorInfo(){
+		System.out.println("Name: "+fName+" "+lName);
+		System.out.println("Region: "+region);
+		System.out.println("Phone: "+phone);
+		System.out.println("Type: "+(type==1?"Vip":"Regular"));
+		System.out.println("Order: "+order.toString());
+	}
+
 }
