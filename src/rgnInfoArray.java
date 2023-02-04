104eb76f1f67fdbd9d7d12e7850343462fa8511e
barhoomi <ialkhayatt@outlook.com>
2023-02-04 13:38:37 +0300

    Remove regionInfoArray.java

--- src/rgnInfoArray.java
+++
@@ -1,9 +0,0 @@
-public class rgnInfoArray {
-    rgnInfo[] data;
-    public rgnInfoArray(int size){
-        data = new rgnInfo[size];
-        for(int i = 0; i < size; i++){
-            data[i] = new rgnInfo();
-        }
-    }
-}
